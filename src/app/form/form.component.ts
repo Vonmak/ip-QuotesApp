5835d2403d1575ba2db0c5e2df82d8de997741d0
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-08 22:19:56 +0300

    Added a snippet code that helps add the new goal to the display.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-form',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
+  newQuote= new Quote(0,'','',new Date)
+
+  @Output()addQuote = new EventEmitter<Quote>()
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
 
   constructor() { }
 
