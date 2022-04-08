c2c004b321049adf950cb8e72b85cae4425717b7
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-08 22:28:57 +0300

    pulled the addnew quote commands to the parent component using the event emit directive.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,6 +15,13 @@ export class QuoteComponent implements OnInit {
     new Quote(3, 'Get a new phone case','Diana has her birthday coming up soon',new Date(2022,1,12)),
   ]
 
+  addNewQuote(quote:any){
+    let qLength = this.quotes.length;
+    quote.id = qLength+1;
+    quote.creationDate=new Date(quote.creationDate)
+    this.quotes.push(quote)
+  }
+
   details(index:number){
     this.quotes[index].showDetails=!this.quotes[index].showDetails
   }
