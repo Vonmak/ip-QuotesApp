2888970d638c0271d8f7f45bb80ade89b38ec872
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-09 00:02:50 +0300

    adde snippet functions to allow the spcified directive to come into action.

--- src/app/quote-highlight.directive.ts
+++ src/app/quote-highlight.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appQuoteHighlight]'
 })
 export class QuoteHighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+
+  private backgroundColor(action:string){
+    this.elem.nativeElement.style.backgroundColor = action;
+  }
+  private color(action:string){
+    this.elem.nativeElement.style.color = action;
+    
+  }
+
+  @HostListener('mouseenter')onMouseEnter(){
+    this.color('white');
+    this.backgroundColor('green')
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.color('');
+    this.backgroundColor('')
+  }
+
+  
 
 }
